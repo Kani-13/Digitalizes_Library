b3b67a457e4bf23cb71aca6b3675b1fe9902bdf4
Kanimozhi13-M <kanimozhim117@gmail.com>
2025-08-25 15:54:30 +0530

    Your commit message here

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// Dummy user
+
 const user = { username: "student", password: "12345" };
 
-// Dummy books database
+
 const books = [
   { id: 1, title: "Data Structures", available: true, dueDate: "2025-09-01" },
   { id: 2, title: "Operating Systems", available: false, dueDate: "2025-08-28" },
   { id: 3, title: "Networking Basics", available: true, dueDate: "2025-09-05" },
-  { id: 4, title: "Web Development", available: false, dueDate: "2025-08-30" }
+  { id: 4, title: "Web Development", available: false, dueDate: "2025-08-30" },
+  { id: 4, title: "Data Science", available: false, dueDate: "2025-08-17" },
+  { id: 4, title: "Web Development", available: false, dueDate: "2025-08-5" },
+  { id: 4, title: "Data Analyst", available: false, dueDate: "2025-08-2" },
+  { id: 4, title: "Software Engineer", available: false, dueDate: "2025-08-30" },
+  { id: 4, title: "CyberSecurity", available: false, dueDate: "2025-08-22" },
+  { id: 4, title: "Ethical Hacking", available: false, dueDate: "2025-08-27" }
 ];
 
-// Login function
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -22,7 +27,7 @@ function login() {
   }
 }
 
-// Search book function
+
 function searchBook() {
   const query = document.getElementById("searchInput").value.toLowerCase();
   const resultsDiv = document.getElementById("results");
@@ -50,7 +55,7 @@ function searchBook() {
   }
 }
 
-// Reminder notification
+
 function sendReminder(title, dueDate) {
   alert(`Reminder set for "${title}". Please return/renew before ${dueDate}.`);
 }
